7b56c08d09a04fd3f5f4b0292a194a405b4fce26
akhilesh2722yadav-glitch <akhilesh2722yadav@gmail.com>
2026-08-23 05:25:43 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Smooth welcome message
+window.onload = function () {
+    console.log("Welcome to Akhil Portfolio");
+};
 
+// Navbar active effect
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", function () {
+        links.forEach(l => l.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
